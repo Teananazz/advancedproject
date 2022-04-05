a0a7305da98858b9e16d84835bd7e4ddf956ba8c
Teananazz <rinshogeki106@gmail.com>
2022-04-05 14:58:31 +0300

    installed bootstrap working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 
 import TextForm from './LoginScreen/TextForm'
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
 // after joke 2
 //after joke 3
 
@@ -11,21 +12,17 @@ const App =
 
         return (
             <>
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                    crossorigin="anonymous"></script>
+                
 
-                <link rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                    crossorigin="anonymous" />
+               
                
             <div className='container'>
                
                <TextForm name = "Kek" />
-
                   
+ 
                 </div>
+                
 
             </>
         )
--- src/LoginScreen/TextForm.js
+++ src/LoginScreen/TextForm.js
@@ -1,14 +1,23 @@
 
+import Button from 'react-bootstrap/Button';
+import React from 'react'
 
- import React from 'react'
 const TextForm =
 
     ({ name }) => {
 
         return (
            <div> 
-                <button type="button" class="btn btn-primary">{name}</button>
-            
+                <button type="button" className="btn btn-primary">{name}</button>
+                <>
+                    <Button variant="primary">Primary</Button>{' '}
+                    <Button variant="secondary">Secondary</Button>{' '}
+                    <Button variant="success">Success</Button>{' '}
+                    <Button variant="warning">Warning</Button>{' '}
+                    <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+                    <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+                    <Button variant="link">Link</Button>
+                </>
              </div>
  
             )
