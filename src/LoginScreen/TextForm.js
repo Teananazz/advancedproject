5b9d6e88e77d8b6240899c70419844eb0d41812c
Teananazz <rinshogeki106@gmail.com>
2022-04-05 15:10:04 +0300

    Removed unnecessery files

--- src/LoginScreen/TextForm.js
+++ src/LoginScreen/TextForm.js
@@ -14,8 +14,10 @@ const TextForm =
                     <Button variant="secondary">Secondary</Button>{' '}
                     <Button variant="success">Success</Button>{' '}
                     <Button variant="warning">Warning</Button>{' '}
-                    <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-                    <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+                    <Button variant="danger">Danger</Button>
+                    <Button variant="info">Info</Button>{' '}
+                    <Button variant="light">Light</Button>
+                    <Button variant="dark">Dark</Button>{' '}
                     <Button variant="link">Link</Button>
                 </>
              </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap';
+
 
 const Element = document.getElementById("root");
 const root = ReactDOM.createRoot(Element);
