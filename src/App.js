8a52099d987326c25d1aa24d663d1d44c1e2f837
Teananazz <rinshogeki106@gmail.com>
2022-04-05 12:41:11 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -12,15 +12,27 @@ const App =
     () => {
 
         return (
-
-            <div className='container'>
+            <>
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
                     crossorigin="anonymous"></script>
 
-                <button type="button" class="btn btn-primary">Primary</button>
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                    crossorigin="anonymous"
+                />
+               
+            
+            <div className='container'>
+               
+             <button type="button" class="btn btn-primary">Primary</button>
 
-            </div>
+                </div>
+
+
+            </>
         )
     }
 
