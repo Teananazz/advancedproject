e7de9f6768ce5f1325bb9d97a0de5356d94a9e8d
Teananazz <rinshogeki106@gmail.com>
2022-04-05 12:53:50 +0300

    sds
    sds

--- src/App.js
+++ src/App.js
@@ -17,20 +17,16 @@ const App =
                     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
                     crossorigin="anonymous"></script>
 
-                <link
-                    rel="stylesheet"
+                <link rel="stylesheet"
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
                     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                    crossorigin="anonymous"
-                />
+                    crossorigin="anonymous" />
                
-            
             <div className='container'>
                
              <button type="button" class="btn btn-primary">Primary</button>
 
                 </div>
-
 
             </>
         )
