9d084b53cfe0b72e7857acff30b01aec2fc4d53a
Teananazz <rinshogeki106@gmail.com>
2022-04-06 00:07:32 +0300

    Primary Login
    Added the beginning of login screen .

--- src/App.js
+++ src/App.js
@@ -3,28 +3,79 @@
 import TextForm from './LoginScreen/TextForm'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import { FiUser } from 'react-icons/fi';
+import { AiFillLock } from 'react-icons/ai';
+
+
+
 // after joke 2
 //after joke 3
+import {useState} from 'react'
+
 
 const App =
     () => {
 
+        const [user, setUser] = useState('')
+        const [password, setPassword] = useState('')
+
+        const Icon_Handler =
+            (name) => {
+
+                if (name == "Username") {
+                    return (<FiUser />)
+                }
+                if (name == "Password") {
+                    return (<AiFillLock />)
+                }
+
+            }
+        const Handler =
+            (e) => {
+                console.log(e)
+            }
         return (
+
             <>
-                
+             <div className="input-group mb-3">
+                    <span className="input-group-text"
+                        id="basic-addon1">   {Icon_Handler("Username")}   </span>
+                    <input type="text"
+                        value={user}
+                        onChange={
+                            (e) => {
+                                setUser(e.target.value)
+                            }
+                        }
+                        className="form-control"
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1" />
+                   </div>
 
-               
-               
-            <div className='container'>
-               
-               <TextForm name = "Kek" />
-                  
- 
-                </div>
-                
 
-            </>
+
+
+             <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">   {Icon_Handler("Password") }   </span>
+                    <input type="text"
+                        value={password}
+                        onChange=
+                        {(e) => {
+                            setPassword(e.target.value)
+                          }
+                        }
+                        className="form-control"
+                        placeholder="Password"
+                        aria-label="Password"
+                        aria-describedby="basic-addon1" />
+                   </div>
+
+             </>
+
+
+
         )
     }
 
--- src/LoginScreen/TextForm.js
+++ src/LoginScreen/TextForm.js
@@ -1,29 +1,41 @@
 
-import Button from 'react-bootstrap/Button';
 import React from 'react'
+import { FiUser } from 'react-icons/fi';
+import { AiFillLock } from 'react-icons/ai';
 
 const TextForm =
 
-    ({ name }) => {
+    ({ name }, props , event) => {
+
+        const Icon_Handler =
+            () => {
+               
+                if (name == "Username") {
+                    return ( <FiUser/>)
+                }
+                if (name == "Password") {
+                    return (<AiFillLock />)
+                }
+                
+            }
+      
 
         return (
-           <div> 
-                <button type="button" className="btn btn-primary">{name}</button>
-                <>
-                    <Button variant="primary">Primary</Button>{' '}
-                    <Button variant="secondary">Secondary</Button>{' '}
-                    <Button variant="success">Success</Button>{' '}
-                    <Button variant="warning">Warning</Button>{' '}
-                    <Button variant="danger">Danger</Button>
-                    <Button variant="info">Info</Button>{' '}
-                    <Button variant="light">Light</Button>
-                    <Button variant="dark">Dark</Button>{' '}
-                    <Button variant="link">Link</Button>
-                </>
-             </div>
- 
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">   {Icon_Handler() }   </span>
+                <input type="text"   className="form-control" placeholder={name} aria-label={name} aria-describedby="basic-addon1"/>
+                   </div>
+
+        
+                
+               
+
+
+
+
             )
 
     }
+
 
 export default TextForm
