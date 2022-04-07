0f2acc474b9c72bba8ffdda6d5b4e8817fa6912f
Teananazz <rinshogeki106@gmail.com>
2022-04-07 13:27:19 +0300

    Fixed register difference

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ const App =
     () => {
         return (
 
-            <LoginScreen />
-            //<RegisterScreen />
+         //   <LoginScreen />
+            <RegisterScreen />
 
         )
 
