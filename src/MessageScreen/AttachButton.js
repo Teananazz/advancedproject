9fb4312ff002c83a337a46a1bb1d8bb0b822af21
Teananazz <rinshogeki106@gmail.com>
2022-04-18 21:08:40 +0300

    Added somethings
    1. You can now submit files.
    2. you can enlarge images.

--- src/MessageScreen/AttachButton.js
+++ src/MessageScreen/AttachButton.js
@@ -6,20 +6,60 @@ import { useState } from 'react'
 
 const AttachButton =
   (props) => {
-    console.log(props)
+     
 
+     
+      
     return (
 
       <>
 
         <input
 
-          type="file" onChange={(e) => {
+                type="file" onChange={(e) => {
+                    var SmallImg = document.getElementById("SmallImg");
+                    var BigImg = document.getElementById("BigImg");
+                    var ModalImg = document.getElementById("ModalImg");
+                  
+
+
+
+
             {/* Using different variables - is it faster ?*/ }
             var Val = e.target.files[0]
             var Val2 = URL.createObjectURL(Val)
-            var FinalVal = <img src={Val2} width="200" height="200" alt={Val.name} />
+                    var FinalVal =
+                        <>
+                            <img id="SmallImg" src={Val2} width="200" height="200" alt={Val.name}
+                                onClick={
+                                    (e) => {
+                                        var ModalImg = document.getElementById("ModalImg");
+                                        var BigImg = document.getElementById("BigImg");
+                                       
+                                        BigImg.src = e.target.src;
+                                        ModalImg.style.display ="block"
+                                        
 
+                                    }
+
+                                }
+                            />
+                              
+                            
+                            <div id="ModalImg" class ="modal">
+
+                                <span class="close" onClick=
+                                    {(e) => {
+                                    var ModalImg = document.getElementById("ModalImg");
+                                    ModalImg.style.display="none"
+
+                                    }}
+                                    
+                                    > &times; </span>
+                            <img class="modal-content" id="BigImg"/>
+                         </div>
+                        </>
+                         
             var phone = props.phone;
             props.Logs.UpdateFileLog({ phone, FinalVal });
             props.func();
@@ -28,8 +68,28 @@ const AttachButton =
 
           className="DropUpButtons cancelInput" id="inputGroupFile01" />
 
-        <label className="DropUpButtons" htmlFor="inputGroupFile01">{IconHandle("Picture")}</label>
-        <label className="DropUpButtons" htmlFor="inputGroupFile01">{IconHandle("Video")}</label>
+            <label className="DropUpButtons" htmlFor="inputGroupFile01">{IconHandle("Picture")}</label>
+
+            <input
+
+                type="file" onChange={(e) => {
+                    {/* Using different variables - is it faster ?*/ }
+                    var Val = e.target.files[0]
+                    var Val2 = URL.createObjectURL(Val)
+                    var FinalVal =
+                        <video width ="320" height = "240" controls>
+                        <source src={Val2} type ="video/mp4" />
+                            </video>
+                    var phone = props.phone;
+                    props.Logs.UpdateFileLog({ phone, FinalVal });
+                    props.func();
+
+                }}
+
+                className="DropUpButtons cancelInput" id="inputGroupFile02" />
+
+
+        <label className="DropUpButtons" htmlFor="inputGroupFile02">{IconHandle("Video")}</label>
       </>
     )
 
